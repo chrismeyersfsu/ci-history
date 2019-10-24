2438211710c09bc2cd2c5804bf581cf2f04f7f80
chris meyers <chris.meyers.fsu@gmail.com>
2019-10-23 21:40:48 -0400

    non-matrix job

--- parser/jenkins_login.py
+++ parser/jenkins_login.py
@@ -5,23 +5,30 @@ import jenkins
 import secrets
 from requests import Request
 
-JOB_NAME = 'Ansible_Tower_Integration_Test'
+JOB_NAME = 'Test_Tower_Integration_Plain'
 
-def get_results(server, base_url, artifact_path):
+def get_results(server, build_id, base_url, artifact_path):
     url = base_url + '/artifact/' + artifact_path
 
     request = Request(method='POST', url=url)
     res = server.jenkins_request(request)
-    print("Found reuslts.xml {}".format(res.content))
-    pass
+    #print("Found reuslts.xml {}".format(res.content))
+    script_path = os.path.dirname(os.path.abspath( __file__ ))
+    with open(os.path.join(script_path, 'sample_data', 'results_{}.xml'.format(build_id)), 'wb+') as f:
+        f.write(res.content)
 
 server = jenkins.Jenkins(secrets.host, username=secrets.username, password=secrets.api_key)
 
 builds = server.get_job_info(JOB_NAME)['builds']
 for b in builds:
+    print(b)
     build_number = b['number']
     build_details = server.get_build_info(JOB_NAME, build_number, depth=2)
-    for r in build_details['runs']:
-        for a in r['artifacts']:
-            if a['fileName'] == 'results.xml':
-                get_results(server, r['url'], a['relativePath'])
+
+    '''
+    Structure for build matrix
+    '''
+    #for r in build_details['runs']:
+    for a in build_details['artifacts']:
+        if a['fileName'] == 'results.xml':
+            get_results(server, build_details['id'], build_details['url'], a['relativePath'])
