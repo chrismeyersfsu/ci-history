b600fb2f360577e3df096d063c865630fa717762
chris meyers <chris.meyers.fsu@gmail.com>
2019-10-27 05:28:06 -0400

    UI supports multiple runs

--- ui/src/index.js
+++ ui/src/index.js
@@ -7,25 +7,9 @@ var C = {
   base_url: window.location.protocol + '//' + window.location.host + '/api/v1/',
   graph: '#graph',
   tooltip: '#tooltip',
-};
-
-// State
-var S = {
-  data: undefined,
-  data_sorted: undefined,
-  tooltip: undefined,
-  tooltip_datapoint_current: undefined,
-  plot: undefined,
-  plot_overview: undefined,
-};
-
-function ep(endpoint) {
-  return C.base_url + endpoint;
-}
-
-$(document).ready(function() {
-
-  var plotOptions = {
+  next: '#next',
+  prev: '#prev',
+  plotOptions: {
     xaxis: {
     },
     grid: {
@@ -43,7 +27,68 @@ $(document).ready(function() {
     selection: {
       mode: "xy"
     }
-  };
+  },
+};
+
+// State
+var S = {
+  runs: undefined,
+  runs_index: 0,
+  data: undefined,
+  tooltip: undefined,
+  plot: undefined,
+  plot_overview: undefined,
+};
+
+function ep(endpoint) {
+  return C.base_url + endpoint;
+}
+
+function get_run(run_id, cb) {
+    $.get(ep('cases/?run=' + run_id), function(data) {
+      S.data = data['results'];
+
+      S.data.sort(function(a, b) {
+        return b.time > a.time ? 1 : -1;
+      });
+      var index = 0;
+      var datapoints = S.data.map(function(entry) {
+        return [index++, entry.time];
+      });
+
+      S.plot = $.plot($(C.graph), [ datapoints ], C.plotOptions);
+      S.plot_overview = $.plot("#graph-overview", [ datapoints ], C.plotOptions);
+
+      if (S.tooltip === undefined) {
+        S.tooltip = new GraphTooltip(S.data, $(C.graph), $(C.tooltip));
+      } else {
+        S.tooltip.data = S.data;
+      }
+
+      if (cb) {
+        cb();
+      }
+    });
+}
+
+$(document).ready(function() {
+
+  $(C.next).bind("click", function(event) {
+    let run_id = S.runs['results'][S.runs_index+1]['_id']['$oid'];
+
+    get_run(run_id, function() {
+      S.runs_index++;
+    });
+  });
+
+  $(C.prev).bind("click", function(event) {
+    let run_id = S.runs['results'][S.runs_index-1]['_id']['$oid'];
+
+    get_run(run_id, function() {
+      S.runs_index--;
+    });
+
+  });
 
   function filterData(from, to) {
     var res = [];
@@ -65,7 +110,7 @@ $(document).ready(function() {
 
     // do the zooming
     S.plot = $.plot(C.graph, [filterData(ranges.xaxis.from, ranges.xaxis.to)],
-      $.extend(true, {}, plotOptions, {
+      $.extend(true, {}, C.plotOptions, {
         xaxis: { min: ranges.xaxis.from, max: ranges.xaxis.to },
         yaxis: { min: ranges.yaxis.from, max: ranges.yaxis.to }
       })
@@ -79,25 +124,10 @@ $(document).ready(function() {
 
   $.get(ep('runs/'), function(data) {
     let run_id = data['results'][0]['_id']['$oid'];
+    S.runs = data;
 
     console.log("Getting run_id " + run_id);
-    $.get(ep('cases/?run=' + run_id), function(data) {
-      S.data = data['results'];
-
-      S.data.sort(function(a, b) {
-        return b.time > a.time ? 1 : -1;
-      });
-      var index = 0;
-      var datapoints = S.data.map(function(entry) {
-        return [index++, entry.time];
-      });
-
-      S.plot = $.plot($(C.graph), [ datapoints ], plotOptions);
-      S.plot_overview = $.plot("#graph-overview", [ datapoints ], plotOptions);
-
-      let tooltip = new GraphTooltip(S.data, $(C.graph), $(C.tooltip));
-
-    });
+    get_run(run_id, null);
   });
 
 
